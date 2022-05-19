1d8c0a334ba08e7c2bc2842eb862ec0384c7f1fd
Ivan Taktaev <32547753+Greaterist@users.noreply.github.com>
2022-05-19 16:53:58 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd
-pacman -S git konsole nano vlc steam kate syncthing firefox dolphin code otf-ipafont
+pacman -S git konsole nano vlc steam kate syncthing firefox dolphin code otf-ipafont kdeconnect
