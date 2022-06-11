5ccbc8c6fffa8ab18d2c3e7a0f05a80d755021a7
Ivan Taktaev <32547753+Greaterist@users.noreply.github.com>
2022-06-11 15:13:11 +0300

    Update UE5.sh

--- fix/UE5.sh
+++ fix/UE5.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 #for error Setting up bundled DotNet SDK
 #/opt/UnrealEngine-5.0.2-release/Engine/Binaries/ThirdParty/DotNet/Linux/sdk/3.1.401/NuGet.targets(255,5): error : Der Dienstindex für die Quelle "https://api.nuget.org/v3/index.json" konnte nicht geladen werden. [/opt/UnrealEngine-5.0.2-release/Engine/Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj]
 
