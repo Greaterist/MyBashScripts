d9ab087ea7753baba57034df3770c23f8b3b9ac5
Ivan Taktaev <32547753+Greaterist@users.noreply.github.com>
2022-07-13 12:19:41 +0300

    Update AIO.sh

--- AIO.sh
+++ AIO.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 wget -O - https://raw.githubusercontent.com/Greaterist/archInstallScript/main/KDE.sh | bash
+wget -O - https://raw.githubusercontent.com/Greaterist/archInstallScript/main/user.sh | bash
 wget -O - https://raw.githubusercontent.com/Greaterist/archInstallScript/main/installer.sh | bash
 wget -O - https://raw.githubusercontent.com/Greaterist/archInstallScript/main/yay.sh | bash
