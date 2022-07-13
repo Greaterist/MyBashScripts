7c3dc790648ed24c97c7d0550a49eade5a860bf2
Ivan Taktaev <32547753+Greaterist@users.noreply.github.com>
2022-07-13 12:34:29 +0300

    Update yay.sh

--- yay.sh
+++ yay.sh
@@ -5,5 +5,7 @@ sudo chown -R greaterist:greaterist ./yay-git
 cd yay-git
 makepkg -si
 cd
-yay -Y --gendb # to generate a development package database for *-git packages that were installed without yay. This command should only be run once.
+yay -Y --gendb 
+yay -S proton-ge-custom --sudoloop
+# to generate a development package database for *-git packages that were installed without yay. This command should only be run once.
 #yay -Y --devel --save #to make development package updates permanently enabled (yay and yay -Syu will then always check dev packages
